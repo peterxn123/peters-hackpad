734578771896de482df2970b24a5768383a874ed
Peter Crossen <125126638+peterxn123@users.noreply.github.com>
2025-08-15 02:46:58 +0300

    Update main.py to make it work

--- FIRMWARE/main.py
+++ FIRMWARE/main.py
@@ -1,8 +1,9 @@
-# hi there! really hope this would work, don't have anything to test it on lol
-
+import digitalio
+import microcontroller
 import board
 import time
-import asyncio
+
+
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
 from kmk.scanners.encoder import RotaryioEncoder
@@ -11,72 +12,69 @@ from kmk.keys import KC
 from kmk.modules.encoder import EncoderHandler
 
 keyboard = KMKKeyboard()
-
-# keys:
 keyboard.matrix = [
     KeysScanner(
         pins=[
-            board.A0,  # GPIO26
-            board.A1,  # GPIO27
-            board.A2,  # GPIO28
-            board.A3,  # GPIO29
-            board.GP7, # GPIO0
-            board.GP0  # GPIO1
+            board.A0,                       # GPIO26
+            board.A1,                       # GPIO27
+            board.A2,                       # GPIO28
+            board.A3,                       # GPIO29
+            board.D7,                       # GPIO7
+            microcontroller.pin.GPIO0,      # GPIO0
+            microcontroller.pin.GPIO4       # Encoder button S1
         ],
         value_when_pressed=False,
         pull=True
     ),
     RotaryioEncoder(
-        pin_a=board.GP2,
-        pin_b=board.GP1
+        pin_a=microcontroller.pin.GPIO2,
+        pin_b=microcontroller.pin.GPIO1
     )
 ]
 
-keyboard.coord_mapping = [
-    0, 1, 2, 3, 4, 5
-]
+keyboard.coord_mapping = [0, 1, 2, 3, 4, 5, 6]
 
-# rgb:
 rgb = RGB(
-    pixel_pin=board.GP6,
+    pixel_pin=microcontroller.pin.GPIO6,
     num_pixels=2,
-    rgb_order=(1, 0, 2),  # GRB
+    rgb_order=(1, 0, 2),
     brightness_limit=255,
 )
 keyboard.extensions.append(rgb)
 
-# encoder:
 encoder_handler = EncoderHandler()
-encoder_handler.encoder_map = [((KC.VOLD, KC.VOLU),)]
+encoder_handler.encoder_map = [
+    ((KC.VOLD, KC.VOLU),)
+]
 keyboard.modules.append(encoder_handler)
 
-# keys:
 keyboard.keymap = [
     [
-        KC.LGUI(KC.D),      # show desktop
-        KC.LGUI(KC.TAB),    # app switcher
-        KC.LGUI(KC.L),      # lock screen
-        KC.LGUI(KC.E),      # open windows explorer 
-        KC.LCTL(KC.C),      # copy
-        KC.LCTL(KC.V),      # paste
+        KC.LGUI(KC.D),
+        KC.LGUI(KC.TAB),
+        KC.LGUI(KC.L),
+        KC.LGUI(KC.E),
+        KC.LCTL(KC.C),
+        KC.LCTL(KC.V),
+        KC.ENTER
     ]
 ]
 
-# blue when turned on
+
 @keyboard.on_startup
 def set_initial_leds():
     base_color = (0, 0, 50)
     rgb.set_rgb_value(0, base_color)
     rgb.set_rgb_value(1, base_color)
 
-# super-duper cool animation on keypress
+
 @keyboard.on_key
 def animate_on_key(key, pressed, **kwargs):
     if pressed:
         pulse_animation()
 
+
 def pulse_animation():
-    # colour goes from blue to white gradually
     steps = 10
     for i in range(steps):
         val = int(255 * (i / steps))
@@ -85,7 +83,11 @@ def pulse_animation():
         time.sleep(0.01)
 
     for i in range(steps):
-        val = int(50 * (1 - i / steps))  # aaand back to blue
+        val = int(50 * (1 - i / steps))
         rgb.set_rgb_value(0, (0, 0, val))
         rgb.set_rgb_value(1, (0, 0, val))
         time.sleep(0.01)
+
+
+if __name__ == '__main__':
+    keyboard.go()
